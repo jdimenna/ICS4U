08d0d9dbdc4be23a8a2d377ba2d9bcb6cd2b77f7
Juliet DiMenna <jdimenna@bayviewglen.ca>
2024-11-15 13:34:11 -0500

    api

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 let students = [
     {id: 1, name: "Juliet", age: 16},
@@ -19,6 +20,16 @@ app.get('/api/students', (req, res) => {
 app.get('/api/students/:id', (req, res) => {
     const student = students.find(s => s.id === parseInt(req.params.id));
     if (!student) return res.status(404).send('Student not found.');
+    res.send(student);
+});
+
+app.post('/api/students', (req, res) => {
+    const student = {
+        id: students.length+1,
+        name: req.body.name,
+        age: req.body.age
+    };
+    students.push(student);
     res.send(student);
 });
 
