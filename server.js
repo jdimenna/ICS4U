1e69eb448d88bd204d89b5f72d91c0574f738581
Juliet DiMenna <jdimenna@bayviewglen.ca>
2024-11-15 13:05:37 -0500

    rest-api

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+let students = [
+    {id: 1, name: "Juliet", age: 16},
+    {id: 2, name: "Simone", age: 16},
+    {id: 3, name: "Isabella", age: 22},
+    {id: 4, name: "Rachael", age: 28},
+]
+
+app.get('/api/students', (req, res) => {
+    res.send(students);
+});
+
+app.get('/api/students/:id', (req, res) => {
+    const student = students.find(s => s.id === parseInt(req.params.id));
+    if (!student) return res.status(404).send('Student not found.');
+    res.send(student);
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
